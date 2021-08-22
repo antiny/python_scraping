1fbf26a27d6eb6093b35838402413a050f24bcdd
antiny <tranminhan.work@gmail.com>
2021-08-23 00:14:18 +0700

    correct param name

--- python_scraping/cli.py
+++ python_scraping/cli.py
@@ -7,7 +7,7 @@ app = typer.Typer()
 
 @app.command()
 def scrape(query: str, count: int = 1000):
-    search_and_download(query, number_images=count)
+    search_and_download(query, count=count)
 
 
 if __name__ == "__main__":
